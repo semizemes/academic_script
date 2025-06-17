ccf9ba9b76a3765005f998c36d0db032d9f3c3a1
semizemes <soatmurodovabbos@gmail.com>
2025-06-17 22:34:19 +0500

    Divide Frontend and Backend part

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
                 <a href="#" className="flex items-center gap-2 self-center font-medium">
-                    <img src='public/jdu-logo-big.svg' />
+                    <img src="/jdu-logo-big.svg" alt="JDU Logo" />
                 </a>
                 <LoginForm />
             </div>
